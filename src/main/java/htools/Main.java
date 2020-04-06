67e4c4928016a861376b9c647c75513a062c1c5b
humberto.machado <humberto.machado@phoebus.local>
2020-04-06 07:59:41 -0300

    Correção de erro de compilação

--- src/main/java/htools/Main.java
+++ src/main/java/htools/Main.java
@@ -7,7 +7,7 @@ import htools.analyzer.XmlIsoAnalyzer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		new XmlIsoAnalyzer().analyze("logiso.xml", args[0]);
+		new XmlIsoAnalyzer().analyze("logiso.xml", "logiso_original.xml", args[0]);
 	}
 
 }
